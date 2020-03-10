3e3ecafbe4a5c14b501f940e46659ad4c07aacb8
azeemnitw <azeemmohd.azeem752@gmail.com>
2020-03-10 11:41:14 +0530

    kadane algo

---
+++ Kadane1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int kadane(int arr[],int n)
+{
+    int max_so_far=0;
+    int max_ending_here=0;
+    for(int i=0;i<n;i++)
+    {
+        max_ending_here+=arr[i];
+        if(max_ending_here<0){
+            max_ending_here=0;
+        }
+        if(max_so_far<max_ending_here){
+            max_so_far=max_ending_here;
+        }
+    }
+    return max_so_far;
+}
+
+int main()
+{
+    int arr[]={-2,-3,4,-1,-2,1,5,-3};
+    cout<<kadane(arr,sizeof(arr)/sizeof(int))<<endl;
+}
